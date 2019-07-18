4ec1737a38b0ef4e0fd4539f6007e30feb93ab4c
유승아 <yoo.seungah@nts-corp.com>
2019-07-18 17:26:29 +0900

    promotion 이미지 로딩

---
+++ src/main/java/com/nts/reservation/service/impl/PromotionServiceImpl.java
@@ -0,0 +1,25 @@
+package com.nts.reservation.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.nts.reservation.dao.PromotionDao;
+import com.nts.reservation.dto.Promotion;
+import com.nts.reservation.service.PromotionService;
+
+@Service
+public class PromotionServiceImpl implements PromotionService{
+	@Autowired
+	PromotionDao promotionDao;
+	
+	
+	@Override
+	@Transactional
+	public List<Promotion> getPromotionImages() {
+		return promotionDao.selectAll();
+	}
+
+}
