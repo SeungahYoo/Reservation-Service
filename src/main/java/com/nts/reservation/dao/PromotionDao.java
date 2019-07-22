8d8ce8b4b2db0806338bd0437aba3c602193dba0
유승아 <yoo.seungah@nts-corp.com>
2019-07-22 10:41:46 +0900

    상수 클래스 제거

--- src/main/java/com/nts/reservation/dao/PromotionDao.java
+++ src/main/java/com/nts/reservation/dao/PromotionDao.java
@@ -2,6 +2,7 @@ package com.nts.reservation.dao;
 
 import java.util.Collections;
 import java.util.List;
+
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -9,24 +10,26 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
-import com.nts.reservation.dto.Promotion;
 
-import static com.nts.reservation.dao.PromotionDaoSqls.*;
+import com.nts.reservation.dto.Promotion;
 
 @Repository
 public class PromotionDao {
 	private NamedParameterJdbcTemplate jdbc;
 	private SimpleJdbcInsert insertAction;
 	private RowMapper<Promotion> rowMapper = BeanPropertyRowMapper.newInstance(Promotion.class);
+	public static final String SELECT_ALL = "SELECT save_file_name promotion_image FROM file_info WHERE id IN "
+		+ "(SELECT file_id FROM product_image WHERE product_id IN"
+		+ "(SELECT id FROM product WHERE id IN "
+		+ "(SELECT product_id FROM promotion)) AND type='th')";
 
 	public PromotionDao(DataSource dataSource) {
 		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
 		this.insertAction = new SimpleJdbcInsert(dataSource)
-				.withTableName("promotion");
+			.withTableName("promotion");
 	}
 
 	public List<Promotion> selectAll() {
 		return jdbc.query(SELECT_ALL, Collections.emptyMap(), rowMapper);
 	}
-
 }
--- src/main/java/com/nts/reservation/dao/PromotionDaoSqls.java
+++
@@ -1,5 +0,0 @@
-package com.nts.reservation.dao;
-
-public class PromotionDaoSqls {
-	public static final String SELECT_ALL = "SELECT save_file_name promotion_image FROM file_info WHERE id IN (SELECT file_id FROM product_image WHERE product_id IN(SELECT id FROM product WHERE id IN (SELECT product_id FROM promotion)) AND type='th')";
-}
