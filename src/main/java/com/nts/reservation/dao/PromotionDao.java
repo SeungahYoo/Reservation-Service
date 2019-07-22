c041feb268986d2ab84712ffb6aa984c214c189b
Seungah Yoo <sayoo0704@gmail.com>
2019-07-22 18:58:05 +0900

    NamedParameterJdbcTemplate Bean으로 변경

--- src/main/java/com/nts/reservation/dao/PromotionDao.java
+++ src/main/java/com/nts/reservation/dao/PromotionDao.java
@@ -3,8 +3,7 @@ package com.nts.reservation.dao;
 import java.util.Collections;
 import java.util.List;
 
-import javax.sql.DataSource;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -18,15 +17,14 @@ public class PromotionDao {
 	private NamedParameterJdbcTemplate jdbc;
 	private SimpleJdbcInsert insertAction;
 	private RowMapper<Promotion> rowMapper = BeanPropertyRowMapper.newInstance(Promotion.class);
-	public static final String SELECT_ALL = "SELECT save_file_name promotion_image FROM file_info WHERE id IN "
+	private static final String SELECT_ALL = "SELECT save_file_name promotion_image FROM file_info WHERE id IN "
 		+ "(SELECT file_id FROM product_image WHERE product_id IN"
 		+ "(SELECT id FROM product WHERE id IN "
 		+ "(SELECT product_id FROM promotion)) AND type='th')";
 
-	public PromotionDao(DataSource dataSource) {
-		this.jdbc = new NamedParameterJdbcTemplate(dataSource);
-		this.insertAction = new SimpleJdbcInsert(dataSource)
-			.withTableName("promotion");
+	@Autowired
+	public PromotionDao(NamedParameterJdbcTemplate jdbc) {
+		this.jdbc = jdbc;
 	}
 
 	public List<Promotion> selectAll() {
