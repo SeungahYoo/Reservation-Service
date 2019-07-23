7004f4e558673fff6e70ebf956113ae0abd9bf04
유승아 <yoo.seungah@nts-corp.com>
2019-07-23 12:16:48 +0900

    category controller 추가

--- src/main/java/com/nts/reservation/dao/CategoryDao.java
+++ src/main/java/com/nts/reservation/dao/CategoryDao.java
@@ -9,23 +9,18 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.nts.reservation.dto.Promotion;
-
 @Repository
-public class PromotionDao {
+public class CategoryDao {
 	private NamedParameterJdbcTemplate jdbc;
-	private RowMapper<Promotion> rowMapper = BeanPropertyRowMapper.newInstance(Promotion.class);
-	private static final String SELECT_ALL = "SELECT save_file_name promotion_image FROM file_info WHERE id IN "
-		+ "(SELECT file_id FROM product_image WHERE product_id IN"
-		+ "(SELECT id FROM product WHERE id IN "
-		+ "(SELECT product_id FROM promotion)) AND type='th')";
+	private RowMapper<String> rowMapper = BeanPropertyRowMapper.newInstance(String.class);
+	private static final String SELECT_ALL = "SELECT name FROM category";
 
 	@Autowired
-	public PromotionDao(NamedParameterJdbcTemplate jdbc) {
+	public CategoryDao(NamedParameterJdbcTemplate jdbc) {
 		this.jdbc = jdbc;
 	}
 
-	public List<Promotion> selectAll() {
+	public List<String> selectAll() {
 		return jdbc.query(SELECT_ALL, Collections.emptyMap(), rowMapper);
 	}
 }
