46bdbe4ad3d9143f9b4145d3df7957be34e66e58
Seungah Yoo <sayoo0704@gmail.com>
2019-07-25 18:57:12 +0900

    @import 제거

--- src/main/java/com/nts/reservation/config/ApplicationConfig.java
+++ src/main/java/com/nts/reservation/config/ApplicationConfig.java
@@ -2,11 +2,9 @@ package com.nts.reservation.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackages = {"com.nts.reservation"})
-@Import({DBConfig.class})
 public class ApplicationConfig {
 
 }
