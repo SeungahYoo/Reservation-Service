4ca281f36689d0f219f1f1ca0a27d3c6d97820c3
유승아 <yoo.seungah@nts-corp.com>
2019-07-29 17:18:10 +0900

    불필요한 import 제거

--- src/main/java/com/nts/reservation/config/DBConfig.java
+++ src/main/java/com/nts/reservation/config/DBConfig.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 @Configuration
 @PropertySource(value = {"classpath:/property/database.properties"})
