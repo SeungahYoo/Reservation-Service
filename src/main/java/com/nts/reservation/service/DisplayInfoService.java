999a3cc6b2dcb1a9e70dd4e30949662db12ec7c6
Seungah Yoo <sayoo0704@gmail.com>
2019-07-31 14:56:29 +0900

    DB로부터 데이터 로드

--- src/main/java/com/nts/reservation/service/DisplayInfoService.java
+++ src/main/java/com/nts/reservation/service/DisplayInfoService.java
@@ -1,10 +1,24 @@
 package com.nts.reservation.service;
 
+import java.util.List;
+
+import com.nts.reservation.dto.Comment;
+import com.nts.reservation.dto.CommentImage;
 import com.nts.reservation.dto.DisplayInfo;
 import com.nts.reservation.dto.DisplayInfoImage;
+import com.nts.reservation.dto.ProductImage;
+import com.nts.reservation.dto.ProductPrice;
 
 public interface DisplayInfoService {
 	DisplayInfo getDisplayInfo(int displayInfoId);
 
 	DisplayInfoImage getDisplayInfoImage(int displayInfoId);
+
+	List<ProductImage> getProductImages(int productId);
+
+	List<ProductPrice> getProductPrices(int productId);
+
+	List<Comment> getComments(int productId);
+
+	List<CommentImage> getCommentImages(int commentId);
 }
--- src/main/java/com/nts/reservation/service/impl/DisplayInfoServiceImpl.java
+++ src/main/java/com/nts/reservation/service/impl/DisplayInfoServiceImpl.java
@@ -1,11 +1,17 @@
 package com.nts.reservation.service.impl;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.nts.reservation.dto.Comment;
+import com.nts.reservation.dto.CommentImage;
 import com.nts.reservation.dto.DisplayInfo;
 import com.nts.reservation.dto.DisplayInfoImage;
+import com.nts.reservation.dto.ProductImage;
+import com.nts.reservation.dto.ProductPrice;
 import com.nts.reservation.mapper.DisplayInfoMapper;
 import com.nts.reservation.service.DisplayInfoService;
 
@@ -26,6 +32,26 @@ public class DisplayInfoServiceImpl implements DisplayInfoService {
 	@Override
 	public DisplayInfoImage getDisplayInfoImage(@Param("displayInfoId") int displayInfoId) {
 		return displayInfoMapper.selectDisplayInfoImage(displayInfoId);
+	}
+
+	@Override
+	public List<ProductImage> getProductImages(int productId) {
+		return displayInfoMapper.selectProductImages(productId);
+	}
+
+	@Override
+	public List<ProductPrice> getProductPrices(int productId) {
+		return displayInfoMapper.selectProductPrices(productId);
+	}
+
+	@Override
+	public List<Comment> getComments(int productId) {
+		return displayInfoMapper.selectComments(productId);
+	}
+
+	@Override
+	public List<CommentImage> getCommentImages(int commentId) {
+		return displayInfoMapper.selectCommentImages(commentId);
 	}
 
 }
