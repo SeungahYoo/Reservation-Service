010cc5d7d797746b966a491dbf47bf961d7d7f6f
유승아 <yoo.seungah@nts-corp.com>
2019-08-06 15:11:16 +0900

    로거 적용

---
+++ src/main/java/com/nts/reservation/controller/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.nts.reservation.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+	@ExceptionHandler({MethodArgumentTypeMismatchException.class, IllegalArgumentException.class})
+	public String handleMethodArgumentTypeMismatch(Exception e) {
+		LOGGER.error(e.getMessage());
+		return "mainpage";
+	}
+
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler({NullPointerException.class})
+	public void handleNullPointerException(Exception e) {
+		LOGGER.error(e.getMessage());
+	}
+}
--- src/main/java/com/nts/reservation/controller/ReservationController.java
+++ src/main/java/com/nts/reservation/controller/ReservationController.java
@@ -1,11 +1,8 @@
 package com.nts.reservation.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @Controller
 public class ReservationController {
@@ -20,11 +17,4 @@ public class ReservationController {
 		return "review";
 	}
 
-	@ExceptionHandler({MethodArgumentTypeMismatchException.class})
-	public String handleMethodArgumentTypeMismatch(
-		MethodArgumentTypeMismatchException ex, WebRequest request) {
-		String error = ex.getName() + " should be of type " + ex.getRequiredType().getName();
-		System.out.println("[Invalid Value] " + error);
-		return "mainpage";
-	}
 }
