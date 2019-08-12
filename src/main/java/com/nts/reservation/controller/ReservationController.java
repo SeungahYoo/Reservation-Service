962e43c10d667402b7675a83f9e8fcfb75f219da
seungah yoo <sayoo0704@gmail.com>
2019-08-13 00:27:08 +0900

    페이지 mapping

--- src/main/java/com/nts/reservation/controller/ReservationController.java
+++ src/main/java/com/nts/reservation/controller/ReservationController.java
@@ -1,6 +1,11 @@
 package com.nts.reservation.controller;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,15 +35,18 @@ public class ReservationController {
 		return "reserve";
 	}
 
-	@PostMapping("reserve")
-	public String saveReserve() {
-		//System.out.println("saveReserve");
-		return "mainpage";
-	}
-
-	@GetMapping("myreservation")
-	public String myreservation() {
+	@GetMapping("my-reservation")
+	public String myReservation(@CookieValue(value = "email", required = false) String email, HttpServletResponse response) {
+		if(email == null) {
+			return "bookinglogin";
+		}
+		
 		return "myreservation";
+	}
+	
+	@GetMapping("booking-login")
+	public String bookinglogin() {
+		return "bookinglogin";
 	}
 
 }
