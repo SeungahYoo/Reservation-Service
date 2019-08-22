0bef93daebaada5f7ca99eb7ecf1365ffc2fa9cf
Seungah Yoo <sayoo0704@gmail.com>
2019-08-22 15:28:43 +0900

    MR 커멘트 반영

--- src/main/java/com/nts/reservation/mapper/CommentMapper.java
+++ src/main/java/com/nts/reservation/mapper/CommentMapper.java
@@ -3,10 +3,12 @@ package com.nts.reservation.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import com.nts.reservation.dto.Comment;
 import com.nts.reservation.dto.CommentImage;
 
+@Repository
 public interface CommentMapper {
 	void insertUserComment(Comment comment);
 
