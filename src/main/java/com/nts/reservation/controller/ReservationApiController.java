a200014280f82ad786b68e86356a3459dc45aa68
유승아 <yoo.seungah@nts-corp.com>
2019-08-22 15:58:27 +0900

    valid 검사 controller에서 service 이동 후 별도  메소드로 분리

--- src/main/java/com/nts/reservation/controller/ReservationApiController.java
+++ src/main/java/com/nts/reservation/controller/ReservationApiController.java
@@ -2,9 +2,7 @@ package com.nts.reservation.controller;
 
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,10 +22,6 @@ import com.nts.reservation.service.ReservationService;
 @RestController
 @RequestMapping(path = "/api")
 public class ReservationApiController {
-	private static final Pattern NAME_PATTERN = Pattern.compile("[^\\s]+");
-	private static final Pattern TELEPHONE_PATTERN = Pattern.compile("^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$");
-	private static final Pattern EMAIL_PATTERN = Pattern.compile(
-		"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
 
 	private final ReservationService reservationService;
 
@@ -57,16 +51,6 @@ public class ReservationApiController {
 
 	@PostMapping("reserve")
 	public ModelAndView saveReserveInfo(@ModelAttribute("reservationParam") ReservationParam reservationParam) {
-		if (StringUtils.isEmpty(reservationParam.getReservationEmail())
-			|| StringUtils.isEmpty(reservationParam.getReservationName())
-			|| StringUtils.isEmpty(reservationParam.getReservationTelephone())
-			|| !EMAIL_PATTERN.matcher(reservationParam.getReservationEmail()).matches()
-			|| !NAME_PATTERN.matcher(reservationParam.getReservationName()).matches()
-			|| !TELEPHONE_PATTERN.matcher(reservationParam.getReservationTelephone()).matches()) {
-
-			throw new IllegalArgumentException("Invalid ReservationParam");
-		}
-
 		reservationService.saveReserveInfo(reservationParam);
 
 		return new ModelAndView("redirect:/user-check");
