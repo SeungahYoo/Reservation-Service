38a82b79824343ac862629c0020c19ff79b710cb
Seungah Yoo <sayoo0704@gmail.com>
2019-08-23 14:20:25 +0900

    MultipartFile 업로드와 가공 로직을 service 에서 controller로 이동

--- src/main/java/com/nts/reservation/controller/CommentApiController.java
+++ src/main/java/com/nts/reservation/controller/CommentApiController.java
@@ -2,6 +2,8 @@ package com.nts.reservation.controller;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,21 +13,68 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.nts.reservation.dto.Comment;
+import com.nts.reservation.dto.CommentImage;
+import com.nts.reservation.helper.FileIOHelper;
 import com.nts.reservation.service.CommentService;
 
 @RestController
 @RequestMapping(path = "/api")
 public class CommentApiController {
-	private final CommentService commentService;
+	private static final String SAVE_PATH = "c:/tmp/";
 
-	public CommentApiController(CommentService commentService) {
+	private final CommentService commentService;
+	private final FileIOHelper fileIOHelper;
+
+	public CommentApiController(CommentService commentService, FileIOHelper fileIOHelper) {
 		this.commentService = commentService;
+		this.fileIOHelper = fileIOHelper;
 	}
 
 	@PostMapping("comments")
-	public ModelAndView saveComment(Comment comment, @RequestParam("files") List<MultipartFile> commentImages)
+	public ModelAndView saveComment(Comment comment, @RequestParam("files") List<MultipartFile> multipartFiles)
 		throws IOException {
+
+		List<CommentImage> commentImages = uploadCommentImages(multipartFiles);
+
 		commentService.saveComment(comment, commentImages);
 		return new ModelAndView("redirect:/user-check");
+	}
+
+	private List<CommentImage> uploadCommentImages(List<MultipartFile> multipartFiles) {
+		List<CommentImage> commentImageList = null;
+
+		commentImageList = multipartFiles.stream()
+			.filter(multipartFile -> !multipartFile.isEmpty())
+			.map(multipartFile -> {
+				CommentImage commentImage = getCommentImage(multipartFile);
+				fileUpload(multipartFile, commentImage.getFileName());
+				return commentImage;
+			})
+			.collect(Collectors.toList());
+
+		return commentImageList;
+	}
+
+	private void fileUpload(MultipartFile multipartFile, String fileName) {
+		try {
+			fileIOHelper.uploadFile(multipartFile, "comment_img/" + fileName);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private CommentImage getCommentImage(MultipartFile multiPartFile) {
+		CommentImage commentImage = new CommentImage();
+
+		String saveFileName = getSaveFileName(multiPartFile.getOriginalFilename());
+		commentImage.setContentType(multiPartFile.getContentType());
+		commentImage.setFileName(saveFileName);
+		commentImage.setSaveFileName(SAVE_PATH + "comment_img/" + saveFileName);
+
+		return commentImage;
+	}
+
+	private String getSaveFileName(String fileName) {
+		return UUID.randomUUID() + fileName.substring(fileName.lastIndexOf("."));
 	}
 }
--- src/main/java/com/nts/reservation/service/impl/CommentServiceImpl.java
+++ src/main/java/com/nts/reservation/service/impl/CommentServiceImpl.java
@@ -2,37 +2,29 @@ package com.nts.reservation.service.impl;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.UUID;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.nts.reservation.dto.Comment;
 import com.nts.reservation.dto.CommentImage;
-import com.nts.reservation.helper.FileIOHelper;
 import com.nts.reservation.mapper.CommentMapper;
 import com.nts.reservation.service.CommentService;
 
 @Service
 public class CommentServiceImpl implements CommentService {
-	private static final String SAVE_PATH = "c:/tmp/";
 
 	private final CommentMapper commentMapper;
-	private final FileIOHelper fileIOHelper;
 
-	public CommentServiceImpl(CommentMapper commentMapper, FileIOHelper fileIOHelper) {
+	public CommentServiceImpl(CommentMapper commentMapper) {
 		this.commentMapper = commentMapper;
-		this.fileIOHelper = fileIOHelper;
+
 	}
 
 	@Override
 	@Transactional
-	public void saveComment(Comment comment, List<MultipartFile> multipartFiles) throws IOException {
+	public void saveComment(Comment comment, List<CommentImage> commentImages) throws IOException {
 		commentMapper.insertUserComment(comment);
-
-		List<CommentImage> commentImages = uploadCommentImages(multipartFiles);
 
 		if (commentImages.isEmpty()) {
 			return;
@@ -48,41 +40,4 @@ public class CommentServiceImpl implements CommentService {
 
 	}
 
-	private List<CommentImage> uploadCommentImages(List<MultipartFile> multipartFiles) {
-		List<CommentImage> commentImageList = null;
-
-		commentImageList = multipartFiles.stream()
-			.filter(multipartFile -> !multipartFile.isEmpty())
-			.map(multipartFile -> {
-				CommentImage commentImage = getCommentImage(multipartFile);
-				fileUpload(multipartFile, commentImage.getFileName());
-				return commentImage;
-			})
-			.collect(Collectors.toList());
-
-		return commentImageList;
-	}
-
-	private void fileUpload(MultipartFile multipartFile, String fileName) {
-		try {
-			fileIOHelper.uploadFile(multipartFile, "comment_img/" + fileName);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	private CommentImage getCommentImage(MultipartFile multiPartFile) {
-		CommentImage commentImage = new CommentImage();
-
-		String saveFileName = getSaveFileName(multiPartFile.getOriginalFilename());
-		commentImage.setContentType(multiPartFile.getContentType());
-		commentImage.setFileName(saveFileName);
-		commentImage.setSaveFileName(SAVE_PATH + "comment_img/" + saveFileName);
-
-		return commentImage;
-	}
-
-	private String getSaveFileName(String fileName) {
-		return UUID.randomUUID() + fileName.substring(fileName.lastIndexOf("."));
-	}
 }
